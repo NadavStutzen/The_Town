f303002bfbb3c891f42a464e5a96e6291dfe12e3
Ran <ran.german555@gmail.com>
2020-04-09 15:12:39 +0300

    added more reducers

---
+++ src/redux/reducers/TownReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    name: '',
+    id: '',
+    users: [],
+    games: [],
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
---
+++ src/redux/reducers/UserReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    id: '',
+    email: '',
+    name: '',
+    towns: [],
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
